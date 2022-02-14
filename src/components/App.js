409b07d27bba70fc2e138ece49b2d615f796d57d
GregT <taylor.gregory2@gmail.com>
2022-02-13 19:21:58 -0700

    Save

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
+
+
 function App() {
+
+  const [sushis, setSushis] = useState([])
+
+useEffect(() => {
+  fetch(API)
+  .then(r => r.json())
+  .then(data => setSushis(data))
+}, [])
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis} />
       <Table />
     </div>
   );
